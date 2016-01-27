da351b2f538a2fca295ca77fbe1c89190b766d95
Elton Santos <eltonaxl@hotmail.com>
2016-01-27 20:53:46 -0300

    Feeding seed

--- app/views/products/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @product, :id, :name, :price, :subcategory_id, :created_at, :updated_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create id: 1, name: "Tecnologia"
+Category.create id: 2, name: "Casa"
+
+Subcategory.create id: 1, name: "Telefonia", category_id: 1
+Subcategory.create id: 2, name: "Informática", category_id: 1
+Subcategory.create id: 3, name: "Móveis", category_id: 2
+Subcategory.create id: 4, name: "Cama, Mesa e Banho", category_id: 2
+Subcategory.create id: 5, name: "Eletrodoméstico", category_id: 2
+
+Product.create id: 1, name: "Zenfone 2", subcategory_id: 1
+Product.create id: 2, name: "Notebook", subcategory_id: 2
+Product.create id: 3, name: "Macbook", subcategory_id: 2
+Product.create id: 4, name: "Mesa", subcategory_id: 3
+Product.create id: 5, name: "Sofá", subcategory_id: 3
+Product.create id: 6, name: "Guarda roupa", subcategory_id: 3
+Product.create id: 7, name: "Toalha", subcategory_id: 4
+Product.create id: 8, name: "Microondas", subcategory_id: 5
+Product.create id: 9, name: "Arcondicionado", subcategory_id: 5
