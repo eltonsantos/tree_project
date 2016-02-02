28fea59e190b4f046c733ab47e0d8f30ec6fa702
Elton Santos <elton@r2a.net.br>
2016-02-02 15:38:10 -0300

    Commit more things

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,12 @@
 class HomeController < ApplicationController
   def index
-    # Mostra todas as categorias
   	@categories = Category.all
-    # Mostra todos as subcategorias
   	@subcategories = Subcategory.all
-    # Mostra todos os produtos
   	@products = Product.all
     puts "Parâmetros: #{params}"
   end
 
   def show_category
-    # Pega o id da categoria
   	@category = Category.find(params[:id])
     @subcategories = @category.subcategories
     @subcategory = Subcategory.find(params[:id])
@@ -28,7 +24,9 @@ class HomeController < ApplicationController
   # Talvez não seja preciso... ou talvez sim
   def show_product
     @category = Category.find(params[:category_id])
+    @subcategories = @category.subcategories
     @subcategory = Subcategory.find(params[:subcategory_id])
+    @products = @subcategory.products
     puts "Parâmetros: #{params}"
   end
 end
